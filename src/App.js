1fa4256efeb079d52ff0b51382dd5754731f292f
unknown <ahmedomar1007@gmail.com>
2022-01-15 13:51:59 +0200

    responsive desgin

--- src/App.js
+++ src/App.js
@@ -3,35 +3,45 @@ import logo from './img/logo.svg'
 
 function App() {
   return (
-    <>
-      <main className='bg-gray-100'>
-        <div className='px-8 py-12'>
-          <img className='h-10' src={logo} alt='Workcation' />
+    <main className='bg-gray-300'>
+      <div className='bg-gray-100 grid lg:grid-cols-2 2xl:grid-cols-5'>
+        <div className='px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2'>
+          <div className='xl:max-w-xl'>
+            <img className='h-10' src={logo} alt='Workcation' />
+            <img
+              className='mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden'
+              src={city}
+              alt='nice city'
+            />
+            <h1 className='mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl'>
+              You can work from anywhere.
+              <br className='hidden lg:inline' />
+              <span className='text-indigo-500'>Take advantage of it.</span>
+            </h1>
+            <p className='mt-2 text-gray-600 sm:mt-4 sm:text-xl'>
+              Workcation helps you find work-friendly rentals in beautiful
+              locations so you can enjoy some nice weather even when you're not
+              on vacation.
+            </p>
+            <div className='mt-4 sm:mt-6'>
+              <a
+                className='inline-block px-5 py-3 rounded-lg bg-indigo-500 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base'
+                href='/'
+              >
+                Book your escape
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className='hidden relative lg:block 2xl:col-span-3'>
           <img
-            className='mt-6 rounded-lg shadow-xl'
+            className='absolute inset-0 w-full h-full object-cover object-center'
             src={city}
             alt='nice city'
           />
-          <h1 className='mt-6 text-2xl font-bold text-gray-900'>
-            You can work from anywhere.
-            <span className='text-indigo-500'>Take advantage of it.</span>
-          </h1>
-          <p className='mt-2 text-gray-600'>
-            Workcation helps you find work-friendly rentals in beautiful
-            locations so you can enjoy some nice weather even when you're not on
-            vacation.
-          </p>
-          <div className='mt-4'>
-            <a
-              className='inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 text-white uppercase tracking-wider font-semibold text-sm'
-              href='/'
-            >
-              Book your escape
-            </a>
-          </div>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   )
 }
 
